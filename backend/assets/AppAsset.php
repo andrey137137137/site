7d62e9b27840281958664c2d944368b26e4ba0b6
Andrey Bannikov <andrey27x777@gmail.com>
2018-04-17 00:32:10 +0300

    Edited ddSlick in backend

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -17,11 +17,11 @@ class AppAsset extends AssetBundle
   public $js = [
     // 'js/lib/jquery-3.2.0.min.js',
     // 'js/lib/jquery-migrate-3.0.0.min.js',
-    'js/lib/jquery.ddslick.min.js',
+    'js/lib/jquery.ddslick.js',
     'js/script.js',
   ];
   public $depends = [
     'yii\web\YiiAsset',
-    'yii\bootstrap\BootstrapAsset',
+    'yii\bootstrap\BootstrapAsset'
   ];
 }
