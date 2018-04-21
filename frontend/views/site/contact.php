e79752ed16a0f3b925d0bcd687db6aea015b63d2
Andrey Bannikov <andrey27x777@gmail.com>
2018-04-21 15:19:41 +0300

    Edited contacts view and backend index.php

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -8,22 +8,14 @@ $this->title = 'Контакты';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<h1><?= Html::encode($this->title) ?></h1>
+  <article class="contacts">
+    <div class="container">
+      <h1><?= Html::encode($this->title) ?></h1>
 
-<div class="wrap-image">
-  <?= Html::img('/img/visit-card.png',
-                    [
-                        'alt' => 'Контакты',
-                        // 'style' => 'width:15px;'
-                    ]
-                ) ?>
-</div>
+      <p>Анна Банникова</p>
+      <p>anka24sp@gmail.com</p>
+      <p>067 711 72 25</p>
+    </div>
+  </article>
 
-<div class="wrap-image">
-  <?= Html::img('/img/visit-card2.png',
-                    [
-                        'alt' => 'Контакты 2',
-                        // 'style' => 'width:15px;'
-                    ]
-                ) ?>
 </div>
