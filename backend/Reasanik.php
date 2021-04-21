a730bc38e0e2dc977085041bdb749e3164c81083
Bannikov Andrey <andrey27x777@gmail.com>
2021-04-21 23:27:40 +0300

    Stylized output

--- backend/Reasanik.php
+++ backend/Reasanik.php
@@ -28,9 +28,46 @@ class Reasanik
       $inputOptions['max'] = $max;
     }
 ?>
+
     <div class="col-md-2">
       <?= Html::label($title . ':', $name, ['class' => 'form-label']) ?>
       <?= Html::input('number', $name, '', $inputOptions) ?>
+    </div>
+  <?php
+  }
+
+  public static function renderOutput($withPercent = false)
+  {
+    $title = $withPercent ? "С процентом" : "Без процента";
+    $plotter = $withPercent ? "plotterWithPercent" : "plotterWithCourse";
+    $list = $withPercent ? "resultListWithPercent" : "resultList";
+    $listTitles = [
+      "Плоттер",
+      "Печать",
+      "Печать, ламинация",
+      "Печать, высечка",
+      "Печать, ламинация, высечка",
+    ];
+    $listStyle = "padding-left: 0;";
+  ?>
+
+    <div class="col-md-6">
+      <h3><?= $title ?></h3>
+      <div class="row">
+        <div class="col-md-6">
+          <ol style="list-style-position: inside; <?= $listStyle ?>">
+            <?php foreach ($listTitles as $value) { ?>
+              <li> <?= $value ?>: </li>
+            <?php } ?>
+          </ol>
+        </div>
+        <div class="col-md-6">
+          <ul style="list-style-type: none; <?= $listStyle ?>">
+            <li> {{<?= $plotter ?>}} </li>
+            <li v-for="item in <?= $list ?>"> {{item.value}} </li>
+          </ul>
+        </div>
+      </div>
     </div>
 <?php
   }
--- backend/views/site/calculator.php
+++ backend/views/site/calculator.php
@@ -29,13 +29,10 @@ Reasanik::renderInput('Ламинация', 'lamination');
 <h2>ВЫВОД</h2>
 
 <?= Reasanik::beginFormGroup() ?>
-<div class="col-md-6">
-  <h4>Без процента</h4>
-  <ol>
-    <li> Плоттер: {{plotterWithCourse}} </li>
-    <li v-for="item in resultList"> {{item.title}}: {{item.value}} </li>
-  </ol>
-</div>
+<?php
+Reasanik::renderOutput();
+Reasanik::renderOutput(true);
+?>
 <?= Reasanik::endFormGroup() ?>
 
 <?= Html::endForm() ?>
